5a42683331d7b4f1754c42552170474eb7672c90
Tushar Deshpande <tushar.deshpande@pubmatic.com>
2015-04-15 11:09:04 +0530

    commiting repo

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularTrainingApp
+ * @description
+ * # angularTrainingApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularTrainingApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
